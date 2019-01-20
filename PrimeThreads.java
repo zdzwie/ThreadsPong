5333ddb0925e0df936c45d32dcf2c80bb1000ef9
Jakub Brzezowski <44787968+zdzwie@users.noreply.github.com>
2019-01-20 21:49:35 +0100

    Add files via upload

---
+++ PrimeThreads.java
@@ -0,0 +1,48 @@
+package Wyjatki;
+
+public class PrimeThreads {
+public static void main(String[] args)
+{
+	PrimeThreads pt = new PrimeThreads(args);
+}
+public PrimeThreads(String[] args)
+{
+	PrimeFinder[] finder = new PrimeFinder[args.length];
+	for(int i = 0;i<args.length;i++) {
+		try {
+			long count = Long.parseLong(args[i]);
+			finder[i] = new PrimeFinder(count);
+			System.out.println("Szukam liczby pierwszej " + count);
+		}
+		catch(NumberFormatException nfe)
+		{
+			System.out.println("B³¹d: "+nfe.getMessage());
+		}
+	}
+
+	boolean complete = false;
+	
+	while(!complete) {
+		complete = true;
+		for(int j = 0; j<finder.length;j++) {
+			if(finder[j]==null)continue;
+			if(!finder[j].finished)complete=false;
+			else {
+				displayResult(finder[j]);
+				finder[j]=null;
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		}
+		catch(InterruptedException ie){
+			
+		}
+	}
+		
+}
+private void displayResult(PrimeFinder finder) {
+	System.out.println("Liczba pierwsza " +finder.target + " to "+ finder.prime);
+}
+
+}
